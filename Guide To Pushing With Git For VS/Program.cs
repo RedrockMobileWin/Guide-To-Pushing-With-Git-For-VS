66df8ef662273c46ec094f4813d462b3a3ab2562
生 叶 <yezhou2@outlook.com>
2016-11-08 00:44:01 +0800

    添加项目文件。

---
+++ Guide To Pushing With Git For VS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guide_To_Pushing_With_Git_For_VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Demo的地址在：https://github.com/RedrockMobileWin/Guide-To-Pushing-With-Git-For-VS");
+        }
+    }
+}
